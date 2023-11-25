a1ffc33ab9e902c4bdb8e01deb820abb3f197e0b
Akerman0509 <strayslugs0509@gmail.com>
2023-11-25 22:46:22 +0700

    first logic commit

---
+++ myFunction.py
@@ -0,0 +1,89 @@
+
+import socket
+import datetime
+import json
+
+'mail1@gmail.com\r\nmail2@gmail.com\r\n'
+
+def getTime():
+    now = datetime.datetime.now()
+    today = datetime.date.today()
+    ans = now.strftime("%H:%M:%S") + " " + today.strftime("%d/%m/%Y")
+    return ans
+
+def toInfoProccess(method, listRecipient):
+    methodMsg = "SendMethod: "+ method + "\r\n"
+    recipientData = ""
+    for i in range (0,len(listRecipient)):
+        recipientData += listRecipient[i] + "\r\n"
+
+    finalMsg = methodMsg + "To: \r\n" + recipientData
+    return finalMsg
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pop3 file
+#format file name yyyymmddhhmmss
+def getFileName(timeInfo):
+    timeInfo = timeInfo.split(" ")
+    time = timeInfo[0].split(":")
+    date = timeInfo[1].split("/")
+    fileName = date[2] + date[1] + date[0] + time[0] + time[1]+ time [2]
+    return fileName
+
+
+
+def saveMail(mailData,usereMail):
+    try:
+        
+        # mail to pypthon dictionary
+
+        # mailData =  "+OK 176\r\nFrom: <codingAkerman@fit.hcmus.edu.vn>\r\nSendMethod: 1\r\nTo: \r\nmail1@gmail.com\r\nmail2@gmail.com\r\nDate: 17:48:51 25/11/2023\r\nSubject: test standard format 1\r\nContent: \r\noijojojoi\r\n.\r\n"
+        dataDict = {}
+        startList = 0
+        endList = 0
+        dataDict["userMail"] = usereMail
+        mailData = mailData.split("\r\n")
+        
+        for i in range (0,len(mailData)):
+            if "from" in mailData[i].lower():
+                dataDict["From"] = mailData[i].split(" ")[1]
+            if "sendmethod" in mailData[i].lower():
+                dataDict["SendMethod"] = mailData[i].split(" ")[1]
+                startList = i + 2
+            if "date" in mailData[i].lower():
+                dataDict["Date"] = mailData[i].split(" ",1)[1]
+                dateInfo = dataDict["Date"]
+                endList = i
+            if "subject" in mailData[i].lower():
+                dataDict["Subject"] = mailData[i].split(" ")[1]           
+            if "content" in mailData[i].lower():
+                dataDict["Content"] = mailData[i+1]
+
+        dataDict["RecipientList"] = mailData[startList:endList]
+        dataDict["Seen"] = 0
+
+        print(json.dumps(dataDict,indent= 6))
+
+
+        # save File
+        fileName = "mailBox\\"+ getFileName(dateInfo)+ ".json"
+        outputFile = open(fileName,"a",encoding='utf-8')
+        
+        json.dump(dataDict,outputFile,indent= 6)
+        outputFile.close()
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return False
+    return True
+
---
+++ pop3.py
@@ -0,0 +1,85 @@
+import socket
+import json
+import myFunction
+
+
+
+pop3Port = 3335
+pop3Sever = "127.0.0.1"
+pop3Addr = (pop3Sever,pop3Port)
+
+# userEmail = "codingAkerman@fit.hcmus.edu.vn"
+userEmail = "mail1@gmail.com"
+
+userPass = "123"
+
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Establist contact to pop3 server {} at port {}".format(pop3Sever,pop3Port))
+clientSocket.connect(pop3Addr)
+
+#check connect fail
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[0:3].lower() == '-err':
+    print('Negative response from server. Stop program')
+
+# USER
+# userEmail = input("Enter your email: ")
+userCmd = "USER {}\r\n".format(userEmail)
+clientSocket.send(userCmd.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[0:3].lower() == '-err':
+    print('Negative response from server. Stop program')
+
+# PASS
+passCmd = "PASS {}\r\n".format(userPass)
+clientSocket.send(passCmd.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[0:3].lower() == '-err':
+    print('Negative response from server. Stop program')
+
+try:
+    # STAT
+    statCmd = "STAT\r\n"
+    clientSocket.send(statCmd.encode())
+    recv = clientSocket.recv(1024).decode()
+    print(recv)
+    if recv[0:3].lower() == '-err':
+        raise Exception ('Negative response from server. Stop program')
+
+    # LIST
+    listCmd = "LIST\r\n"
+    clientSocket.send(listCmd.encode())
+    recv = clientSocket.recv(1024).decode()
+    print(recv)
+    if recv[0:3].lower() == '-err':
+        raise Exception('Negative response from server. Stop program')
+
+    #RETR
+
+    for i in range (1,2):
+        retrCmd = "RETR {}\r\n".format(input("Enter mail your want to read : "))
+        clientSocket.send(retrCmd.encode())
+        recv = clientSocket.recv(1024).decode()
+        print ("-------------------------Data = ",recv)
+        if recv[0:4].lower() == '-err':
+            raise Exception('Negative response from server. Stop program')
+        
+        if(myFunction.saveMail(recv,userEmail)):
+            print("Save mail successfully")
+        else : 
+            print("Can't save mail")
+
+except Exception as e:
+    print("Error occurred: ",e)
+    
+finally:
+    # QUIT
+    quitCmd = "QUIT\r\n"
+    clientSocket.send(quitCmd.encode())
+    recv = clientSocket.recv(1024)
+
+
